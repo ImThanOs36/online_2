159fa3f75062ebec963283e8bd813a39b7765dc4
ThanOs <lads42279@gmail.com>
2023-04-08 02:45:40 +0530

    responsive design done

---
+++ Script/script.js
@@ -0,0 +1,24 @@
+var show = document.getElementById("show");
+var close = document.getElementById("close");
+
+var hide_button =document.getElementById("header__navigation__second");
+
+
+
+
+function show_hide(){
+    show.addEventListener("click", function run(){
+
+        hide_button.style.display="block";
+        close.style.display="block"
+        show.style.display="none"
+    });
+    close.addEventListener("click", function run(){
+        
+        hide_button.style.display="none";
+        show.style.display="block"
+        close.style.display="none"
+    });
+
+
+}
