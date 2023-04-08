e5e6458db86fb55e348769fb7ccaaed4b3d58226
ThanOs <lads42279@gmail.com>
2023-04-08 11:39:47 +0530

    testing

--- Script/script.js
+++ Script/script.js
@@ -4,7 +4,7 @@ var close = document.getElementById("close");
 var hide_button =document.getElementById("header__navigation__second");
 
 
-
+console.log("Hellow")
 
 function show_close(){
     show.addEventListener("click", function run(){
@@ -13,12 +13,12 @@ function show_close(){
         close.style.display="block"
         show.style.display="none"
     });
-    close.addEventListener("click", function run(){
+    // close.addEventListener("click", function run(){
         
-        hide_button.style.display="none";
-        show.style.display="block"
-        close.style.display="none"
-    });
+    //     hide_button.style.display="none";
+    //     show.style.display="block"
+    //     close.style.display="none"
+    // });
 
 
 }
