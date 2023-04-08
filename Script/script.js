29b83519a63b583225d88fa159a1fa23df2b94ae
ThanOs <lads42279@gmail.com>
2023-04-08 11:26:53 +0530

    initial commit

--- Script/script.js
+++ Script/script.js
@@ -6,15 +6,13 @@ var hide_button =document.getElementById("header__navigation__second");
 
 
 
-function show(){
+function show_close(){
     show.addEventListener("click", function run(){
 
         hide_button.style.display="block";
         close.style.display="block"
         show.style.display="none"
     });
-}
-function close(){
     close.addEventListener("click", function run(){
         
         hide_button.style.display="none";
