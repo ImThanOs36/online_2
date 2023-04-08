e0a4ef16eccb163e13beaa693c29cd1016d18d4f
ThanOs <lads42279@gmail.com>
2023-04-08 11:23:00 +0530

    script updated

--- Script/script.js
+++ Script/script.js
@@ -6,13 +6,15 @@ var hide_button =document.getElementById("header__navigation__second");
 
 
 
-function show_hide(){
+function show(){
     show.addEventListener("click", function run(){
 
         hide_button.style.display="block";
         close.style.display="block"
         show.style.display="none"
     });
+}
+function close(){
     close.addEventListener("click", function run(){
         
         hide_button.style.display="none";
